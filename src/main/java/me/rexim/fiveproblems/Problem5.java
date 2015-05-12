488ed8047d0fa95c6626152b9eeb055b7d326a5e
rexim <reximkut@gmail.com>
2015-05-12 12:27:25 +0600

    Simplified the fifth solution. Close #2

--- src/main/java/me/rexim/fiveproblems/Problem5.java
+++ src/main/java/me/rexim/fiveproblems/Problem5.java
@@ -1,15 +1,16 @@
 package me.rexim.fiveproblems;
 
 import java.util.ArrayList;
+import static java.lang.Math.*;
 
 public class Problem5 {
     public static String[] possibleExpressions() {
         ArrayList<String> result = new ArrayList<>();
-        possibleExpressionsImpl(2, 1, 1, "+", "1", result);
+        possibleExpressionsImpl(2, 1, 1, "1", result);
         return result.toArray(new String[result.size()]);
     }
 
-    private static void possibleExpressionsImpl(int i, int sum, int f, String op, String expr, ArrayList<String> acc) {
+    private static void possibleExpressionsImpl(int i, int sum, int f, String expr, ArrayList<String> acc) {
         if (i > 9) {
             if (sum == 100) {
                 acc.add(expr);
@@ -17,8 +18,12 @@ public class Problem5 {
             return;
         }
 
-        possibleExpressionsImpl(i + 1, sum + i, i, "+", expr + " + " + i, acc);
-        possibleExpressionsImpl(i + 1, sum - i, i, "-", expr + " - " + i, acc);
-        possibleExpressionsImpl(i + 1, sum + (f * 9 + i) * (op.equals("-") ? -1 : 1), f * 10 + i, op, expr + i, acc);
+        possibleExpressionsImpl(i + 1, sum + i, i, expr + " + " + i, acc);
+        possibleExpressionsImpl(i + 1, sum - i, -i, expr + " - " + i, acc);
+        possibleExpressionsImpl(i + 1, sum + f * 9 + i * dir(f), f * 10 + i * dir(f), expr + i, acc);
+    }
+
+    private static int dir(int x) {
+        return x / abs(x);
     }
 }
